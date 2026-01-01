1ff332fb513c37556744ef57bc4c42ec376e93b9
Andrew Birch <ab8218-cp@yahoo.com>
2026-01-01 13:02:39 +0000

    BUILD: app javascript

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const str = localStorage.getItem("stats") ? localStorage.getItem("stats") : null;
 // console.log(str); // check local storage
-const pageText = document.getElementById("pageText"); // page text
+const pageText = document.getElementById("pageText");
+let userData = {};
+const coLours = ["light blue", "pink", "green", "purple", "red", "black", "brown", "white", "light green", "orange", "silver", "blue", "yellow", "beige"];
+const favCol = coLours[rNum(coLours.length)];
+const aGe = (rNum(100)) + 2;
 
 // presents ages 1-5, 6-12, 13-20, 21-35, 36-50, 50+
 // const mPres = [{}, {}, {}, {}, {}, {}];
@@ -38,27 +42,25 @@ function createElem(e, aN, pI, aT, iTx, typ, f, req) {
   const el = document.createElement(e);
   // const mAin = document.getElementsByTagName("main");
   const secTion = document.getElementsByTagName("section");
-  if (pI) { // parent from id
+  if (pI) {
     par = document.getElementById(pI);
   } else {
-    // par = mAin[0]; // default parent
-    par = secTion[((secTion.length) - 1)]; // default parent
+    // par = mAin[0];
+    par = secTion[((secTion.length) - 1)];
   }
-  if (aN) { // add class or id
+  if (aN) {
     if (aT == "c") { el.className = aN; } else { el.id = aN; }
     if (e === "input") { el.name = aN; if (req) { el.required = true; } }
   }
   if (iTx) { el.innerText = iTx; }
   if (typ) { el.type = typ; }
   if (f) { el.htmlFor = f; }
-
   par.appendChild(el);
   return el;
 }
 
-function storeValues(K, val) { // save to local storage // key / value pairs
+function storeValues(K, val) {
   let V = JSON.stringify(val);
-  console.log(`string from storage ${str}`);
   localStorage.setItem(K, V);
 }
 
@@ -66,81 +68,87 @@ function rNum(X) {
   return Math.floor(Math.random() * X);
 }
 
-function removeEl(el, func) { // delay remove element
+function removeEl(el, func) {
   setTimeout(() => el.remove(), 5000);
-  // callback if exists // calback must be delayed more than 5000ms
+  // must delay callback >5000ms
   if (func) { func(); }
 }
 
-// favourite colours
-const coLours = ["light blue", "pink", "green", "purple", "red", "black", "brown", "white", "light green", "orange", "silver", "blue", "yellow", "beige"];
-const favCol = coLours[rNum(coLours.length)];
-// random age
-const aGe = (rNum(100)) + 1;
-
 function formTwo() {
+  pageText.innerText = `Hello, ${userData.userName}.\r\n\nThank you for submitting your data.\r\n\nToday we will be collating information that you provide together with some very clever AI generated content for us to determine the perfect Christmas present for you.`;
   // elememt, attr.name, parent id, attr.type, inner text, type, for, required
-  createElem("p", "", "", "", "You will be pleased to know, you have passed our initial checks and you are now able to proceed and find your perfect Christmas present.", "", "", "");
-  createElem("p", "", "", "", "A few more steps are now required....", "", "", "");
-  // Add large, red (white text?) 'your present' button? for present
-
-  const foRm = createElem("form", "dataForm", "", "", "", "", "", "");
+  createElem("p", "p1", "", "", "You will be pleased to know, you have passed our initial checks and you are now able to proceed and find your perfect Christmas present.", "", "", "");
+  createElem("p", "p2", "", "", "A few more steps are now required....", "", "", "");
+  createElem("form", "dataForm", "", "", "", "", "", "");
   createElem("label", "", "dataForm", "", "What is your favourite colour?", "", "favColour", "");
   createElem("input", "favColour", "dataForm", "", "", "text", "", "y");
   createElem("label", "", "dataForm", "", "How old are you?", "", "howOld", "");
   createElem("input", "howOld", "dataForm", "", "", "number", "", "");
   createElem("button", "", "dataForm", "", "submit", "submit", "", "");
 
+  const p1 = document.getElementById("p1");
+  const p2 = document.getElementById("p2");
+  // const favColour = document.getElementById("favColour"); // fav colour input
+  // const howOld = document.getElementById("howOld"); // age input
+  // const teL = document.getElementById('telNum'); // telephone input
+
   dataForm.addEventListener("submit", (e) => {
     e.preventDefault(); // prevent data from showing in url
     const formData = new FormData(dataForm);
     const dataObj = Object.fromEntries(formData);
-    console.log(dataObj);
+    // console.log(dataObj);
 
-    // update form values
+    // change form values
     howOld.value = aGe;
     favColour.value = favCol;
-
-    // update object values
-    dataObj.howOld = aGe;
-    dataObj.favColour = favCol;
-
-    // todo: combine new stats with local storage
-    // console.log(dataObj);
-    // storeValues("stats", dataObj);
-
-    // pageText.innerText = `Hello ${uSer.name}, thank you for submitting your data. Today we will be collating information that you provide together with some very clever AI generated content for us to determine the perfect Christmas present for you.`;
+   
+    // add user stats
+    userData.howOld = aGe;
+    userData.favColour = favCol;
+    storeValues("stats", userData);
 
     // delay, run another function after removing form??
     // re-use function for other elements' removal
     // removeEl(dataForm, () => setTimeout(console.log("run new function"), 6000));
-    removeEl(dataForm, () => setTimeout(formTwo, 5100));
-    // removeEl(dataForm, null);
-    // alert("Did you enter the correct name? An error was detected!");
+    // removeEl(dataForm, () => setTimeout(formTwo, 5100));
+    // todo: remove list of elements?
+    // const toRemove = [dataForm, p1, p2]; // [...args]
+    // removeEl(toRemove, null);
+    removeEl(dataForm, null);
+    removeEl(p1, null);
+    removeEl(p2, () => setTimeout(funcThree, 5100));
+    // removeEl(p2, null);
+    // pageText.innerText="";
 
   });
 }
 
+function funcThree() {
+  pageText.innerText = "new function running";
+  // add telephone number js, We have your current location and together with the information you have provided, we are able to determine your telephone number. We have added this to your collated data and we may use this to contact you to confirm your personal details. Your telephone number is - 
+  // amend personal details button
+  // Add large, red button with white text for present??
+}
+
 if (str) { // stats exist in local storage
   try {
-    const obj = JSON.parse(str);
-    // userName = obj.userName;
-    // favColour = obj.favColour;
-    // howOld = obj.howOld;
-    console.log(`object from storage ${obj}, ${obj.userName}, ${obj.favColour}, ${obj.howOld}`);
+    userData = JSON.parse(str);
+    console.log(userData);
+    // userName = userData.userName;
+    // favColour = userData.favColour;
+    // howOld = userData.howOld;
+    // console.log(`object from storage ${userData}, ${userData.userName}, ${userData.favColour}, ${userData.howOld}`);
   } catch (err) {
-    console.error(err);
+    console.error(`local storage error: ${err}`);
   }
 
+  // todo: local storage exists, SAY HI AND LIST STATS FROM LAST VISIT, add 'amend' details button
+  pageText.innerText = `Welcome back!\r\n\nFrom your previous visit we harvested the following personal information:\r\n\nYour name is ${userData.userName}\r\nYour favourite colour is ${userData.favColour}\r\nYou are ${userData.howOld} years of age\r\nYour gender is ${userData.userGen}`;
 
-  // todo: local storage exists, SAY HI AND LIST STATS FROM LAST VISIT, add 'amend' details button 
 
-
-} else { // nothing in local storage
-
-  // create form ==================================
+} else { // local storage empty -> create form
   // elememt, attr.name, parent id, attr.type, inner text, type, for, required
-  const foRm = createElem("form", "dataForm", "", "", "", "", "", "");
+  createElem("form", "dataForm", "", "", "", "", "", "");
   createElem("label", "", "dataForm", "", "What is your name?", "", "userName", "");
   createElem("input", "userName", "dataForm", "", "", "text", "", "y");
   const chEck = createElem("p", "", "dataForm", "", "", "", "", "");
@@ -151,12 +159,9 @@ if (str) { // stats exist in local storage
   createElem("input", "feMale", "dataForm", "", "", "checkbox", "", "");
   createElem("button", "", "dataForm", "", "submit", "submit", "", "");
 
-  // const favColour = document.getElementById("favColour"); // fav colour input
   const userName = document.getElementById("userName"); // username input
-  // const howOld = document.getElementById("howOld"); // age input
   const m = document.getElementById('maLe'); // male checkbox
   const f = document.getElementById('feMale'); // female checkbox
-  // const teL = document.getElementById('telNum'); // telephone input
   const dataForm = document.getElementById("dataForm"); // form
 
   m.addEventListener("input", () => { f.checked = false; });
@@ -196,6 +201,15 @@ if (str) { // stats exist in local storage
   // function popHurry() {
   // }
 
+  // todo: add annoying alerts with setInterval? -> alert("Did you enter the correct name? An error was detected!");
+
+  // todo: get telephone number after form submission, add number to form Object? Get from local storage
+  // teL.addEventListener("click", () => {
+  //   teL.value = "999";
+  //   const timer = setInterval(() => { teL.value = ""; alert("Thanks"); clearInterval(timer);}, 5000);
+  //   popup - Thank you. Your telephone number has been submitted, we may use your number to contact you in the near future. We need this to confirm your personal information.
+  // });
+
   // todo: CREATE FAVOURITE MOVIE 
   // from the information you have submitted and with the clever use of generative AI, we can determine that -
   // your favourite Christmas movie is - the King's speech, Die Hard 2, Indiana Jones and the Temple of Doom, Mary Poppins
@@ -205,9 +219,7 @@ if (str) { // stats exist in local storage
   // submit(); reset();
 
   // todo: CREATE FAVOURITE CHRISTMAS PRESENT
-  // your favourite Christmas present is - 
-
-
+  // your favourite Christmas present is -  
 
 
   dataForm.addEventListener("submit", (e) => {
@@ -217,44 +229,41 @@ if (str) { // stats exist in local storage
     // console.log(dataObj);
 
     // get gender from form, select name from array
-    let uSer;
+    let uSer, gEnder;
     switch ("on") {
       case dataObj.maLe:
         uSer = nMale[rNum(nMale.length)];
+        gEnder = "male";
         break;
       case dataObj.feMale:
         uSer = nFemale[rNum(nFemale.length)];
+        gEnder = "female";
         break;
-      default: uSer = nFemale[rNum(nFemale.length)];
+      default:
+        uSer = nFemale[rNum(nFemale.length)];
+        gEnder = "unconfirmed";
     }
 
-    // update form values
+    // change form values
     userName.value = uSer.name;
 
-    // update object values
-    dataObj.userName = uSer.name;
-
-    // console.log(dataObj);
-    storeValues("stats", dataObj);
-
-    pageText.innerText = `Hello ${uSer.name}, thank you for submitting your data. Today we will be collating information that you provide together with some very clever AI generated content for us to determine the perfect Christmas present for you.`;
+    // add user data
+    userData.userName = uSer.name;
+    userData.userGen = gEnder;
+    storeValues("stats", userData);
 
     // delay, run another function after removing form??
     // re-use function for other elements' removal
-    // removeEl(dataForm, () => setTimeout(console.log("run new function"), 6000));
+    // removeEl(dataForm, () => setTimeout(console.log("run new function"), 6000)); // test callback
     removeEl(dataForm, () => setTimeout(formTwo, 5100));
-    // removeEl(dataForm, null); 
+    // removeEl(dataForm, null); // remove element, no callback
+    // removing element also removes this event handler
 
   });
-
-  // todo: get telephone number after form submission, add number to form Object? Get from local storage
-  // teL.addEventListener("click", () => {
-  //   teL.value = "999";
-  //   const timer = setInterval(() => { teL.value = ""; alert("Thanks"); clearInterval(timer);}, 5000);
-  //   popup - Thank you. Your telephone number has been submitted, we may use your number to contact you in the near future. We need this to confirm your personal information.
-  // });
-
 }
+
+
+
 
 // CUSTOM POPUP BOX =================
 // play.addEventListener("click", () => {
