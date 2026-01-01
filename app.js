5d5c901f1aa5a5938d08319f02872d2ee129d77a
Andrew Birch <ab8218-cp@yahoo.com>
2026-01-01 17:33:17 +0000

    BUILD: app javascript

--- app.js
+++ app.js
@@ -6,46 +6,94 @@ const coLours = ["light blue", "pink", "green", "purple", "red", "black", "brown
 const favCol = coLours[rNum(coLours.length)];
 const aGe = (rNum(100)) + 2;
 
-// presents ages 1-5, 6-12, 13-20, 21-35, 36-50, 50+
-// const mPres = [{}, {}, {}, {}, {}, {}];
-// const fPres = [{}, {}, {}, {}, {}, {}];
-// switch (true) {
-//   case age < 6:
+const nMale = [
+  { name: "Noel" },
+  { name: "Joseph" },
+  { name: "Gabriel" },
+  { name: "Jesus" },
+  { name: "Rudolph" },
+  { name: "Ebenezer Scrooge" },
+  { name: "Buddy the Elf" },
+  { name: "Santa Claus" }
+];
 
-//     break;
-//   case age > 5 && age < 13:
+const nFemale = [
+  { name: "Mary" },
+  { name: "Carol Singer" },
+  { name: "Holly" },
+  { name: "Angel" },
+  { name: "Noelle" },
+  { name: "Ivy" }
+];
 
-//     break;
-//   case age > 12 && age < 21:
+function getGift(age, gen) {
+  // console.log(`gift age ${age}`);
+  // console.log(`gift gender ${gen}`);
+  // presents ages 2-5, 6-12, 13-20, 21-35, 36-50, 50+
+  let preSent;
+  const mPres = [
+    ["Heavy Goods Vehicle","VW Beetle","shovel","pet baboon","uni-cycle","pair of clown shoes","spanner","driving experience","pipe and tobacco","punch ball"],
+    ["bottle","horror dvd","walking stick","pet reindeer","bottle of whiskey","flying Lesson","bottle opener","tea pot","pair of walking boots","hip flask"],
+    ["pet penguin","air fryer","cutlery set","National Trust membership","real polar bear","pet lion","log cabin","cement mixer","lawn mower","scented candle"],
+    ["SpongeBob SquarePants","toy ferrari","wordsearch book","jigsaw puzzle","Scrabble Set","calendar","pen set","can of oil","drawing pad","hair straightener"],
+    ["broom","pogo stick","pack of Paracetamol","Thomas the Tank Engine","colouring book","crossword puzzle book","bottle of diet pills","Beano Annual","Guinness Book of Records","Space Hopper"],
+    ["colouring book","action man","pair of roller blades","pair of clogs","mini skirt","plastic sheep","personalised mug","gym membership","Lego set","Play-Doh set"]
+  ];
+  const fPres = [
+    ["Prosecco","bottle of Gin","hairdryer","spa day","comb","food mixer","manicure set","bottle of Chanel No.5","tin of biscuits","tub of Quality Street"],
+    ["set of hair curlers","flower boquet","greenhouse","chopping board","socket set","bottle of diet pills","gym membership","flower pot","Slinky","Nintendo Switch"],
+    ["Dyson Vacuum Cleaner","baking tray","bricklaying course","screwdriver set","personalised toothbrush","pair of novelty socks","snack hamper","bicycle tyre","Cluedo Board Game","portable power bank"],
+    ["set of bowls","cricket bat","Furby","Star Wars Power Crystal Lightsaber","Hasbro Frustration Board Game","Scalextric Set","train set","Airfix Kit","Evel Knievel Stunt Set","Stretch Armstrong"],
+    ["lipstick","perm","blue rinse","self-help book","radio-controlled car","build-a-bear workshop","Rubiks Cube","Monopoly Game","Sylvanian Families playset","Spirograph Set"],
+    ["bicycle","Barbie Doll","pet hamster","set of novelty underwear","Fart Box","Christmas jumper","tricycle","football","Nintendo Game Boy","Jenga Game"]
+  ];
 
-//     break;
-//   case age > 20 && age < 36:
+  switch (true) {
+    case age < 6:
+      ranGe = 0;
+      break;
+    case age > 5 && age < 13:
+      ranGe = 1;
+      break;
+    case age > 12 && age < 21:
+      ranGe = 2;
+      break;
+    case age > 20 && age < 36:
+      ranGe = 3;
+      break;
+    case age > 35 && age < 51:
+      ranGe = 4;
+      break;
+    case age > 49:
+      ranGe = 5;
+      break;
+  }
 
-//     break;
-//   case age > 35 && age < 51:
+  if (gen === "male") {
+    preSent = mPres[ranGe][rNum(mPres[ranGe].length)];
+    // return preSent;
+    console.log(preSent);
+  }
 
-//     break;
-//   case age > 49:
+  if (gen === "female" || gen === "unconfirmed") {
+    preSent = fPres[ranGe][rNum(fPres[ranGe].length)];
+    // return preSent;
+    console.log(preSent);
+  }
 
-//     break;
-// }
-
-// preSents[1];
-// preSents[2];
-// preSents[3];
-// preSents[4];
-// preSents[5];
+  // add user stats
+  userData.giFt = preSent;
+  storeValues("stats", userData);
+}
 
 
 function createElem(e, aN, pI, aT, iTx, typ, f, req) {
   let par;
   const el = document.createElement(e);
-  // const mAin = document.getElementsByTagName("main");
   const secTion = document.getElementsByTagName("section");
   if (pI) {
     par = document.getElementById(pI);
   } else {
-    // par = mAin[0];
     par = secTion[((secTion.length) - 1)];
   }
   if (aN) {
@@ -76,9 +124,8 @@ function removeEl(el, func) {
 
 function formTwo() {
   pageText.innerText = `Hello, ${userData.userName}.\r\n\nThank you for submitting your data.\r\n\nToday we will be collating information that you provide together with some very clever AI generated content for us to determine the perfect Christmas present for you.`;
-  // elememt, attr.name, parent id, attr.type, inner text, type, for, required
   createElem("p", "p1", "", "", "You will be pleased to know, you have passed our initial checks and you are now able to proceed and find your perfect Christmas present.", "", "", "");
-  createElem("p", "p2", "", "", "A few more steps are now required....", "", "", "");
+  createElem("p", "p2", "", "", "A few more steps are required....", "", "", "");
   createElem("form", "dataForm", "", "", "", "", "", "");
   createElem("label", "", "dataForm", "", "What is your favourite colour?", "", "favColour", "");
   createElem("input", "favColour", "dataForm", "", "", "text", "", "y");
@@ -88,20 +135,21 @@ function formTwo() {
 
   const p1 = document.getElementById("p1");
   const p2 = document.getElementById("p2");
+  const butt = document.getElementsByTagName("button");
   // const favColour = document.getElementById("favColour"); // fav colour input
   // const howOld = document.getElementById("howOld"); // age input
   // const teL = document.getElementById('telNum'); // telephone input
 
   dataForm.addEventListener("submit", (e) => {
+    butt[0].disabled = true;
     e.preventDefault(); // prevent data from showing in url
     const formData = new FormData(dataForm);
     const dataObj = Object.fromEntries(formData);
-    // console.log(dataObj);
 
     // change form values
     howOld.value = aGe;
     favColour.value = favCol;
-   
+
     // add user stats
     userData.howOld = aGe;
     userData.favColour = favCol;
@@ -111,7 +159,7 @@ function formTwo() {
     // re-use function for other elements' removal
     // removeEl(dataForm, () => setTimeout(console.log("run new function"), 6000));
     // removeEl(dataForm, () => setTimeout(formTwo, 5100));
-    // todo: remove list of elements?
+    // todo: remove a list of elements?
     // const toRemove = [dataForm, p1, p2]; // [...args]
     // removeEl(toRemove, null);
     removeEl(dataForm, null);
@@ -124,10 +172,19 @@ function formTwo() {
 }
 
 function funcThree() {
-  pageText.innerText = "new function running";
+  pageText.innerText = `${userData.userName}, if you are not completely happy with the data you have submitted you may now amend your details, or get your perfect present!`;
   // add telephone number js, We have your current location and together with the information you have provided, we are able to determine your telephone number. We have added this to your collated data and we may use this to contact you to confirm your personal details. Your telephone number is - 
   // amend personal details button
   // Add large, red button with white text for present??
+  const amendBtn = createElem("div", "btn", "", "c", "amend", "", "", "");
+  const giftBtn = createElem("div", "btn", "", "c", "present", "", "",);
+  amendBtn.title = "amend personal details";
+  giftBtn.title = "perfect present idea";
+  giftBtn.addEventListener("click", () => {
+    // console.log(`age ${userData.howOld}`);
+    // console.log(`gender ${userData.userGen}`);
+    getGift(userData.howOld, userData.userGen);
+  });
 }
 
 if (str) { // stats exist in local storage
@@ -143,7 +200,7 @@ if (str) { // stats exist in local storage
   }
 
   // todo: local storage exists, SAY HI AND LIST STATS FROM LAST VISIT, add 'amend' details button
-  pageText.innerText = `Welcome back!\r\n\nFrom your previous visit we harvested the following personal information:\r\n\nYour name is ${userData.userName}\r\nYour favourite colour is ${userData.favColour}\r\nYou are ${userData.howOld} years of age\r\nYour gender is ${userData.userGen}`;
+  pageText.innerText = `Welcome back!\r\n\nFrom your previous visit we harvested the following personal information:\r\n\nYour name is ${userData.userName}\r\nYour favourite colour is ${userData.favColour}\r\nYou are ${userData.howOld} years of age\r\nYour gender is ${userData.userGen}\r\n\nFrom the information you provided and our clever use of generative AI, we concluded that your perfect Christmas present was a ${userData.giFt}`;
 
 
 } else { // local storage empty -> create form
@@ -163,70 +220,43 @@ if (str) { // stats exist in local storage
   const m = document.getElementById('maLe'); // male checkbox
   const f = document.getElementById('feMale'); // female checkbox
   const dataForm = document.getElementById("dataForm"); // form
+  const butt = document.getElementsByTagName("button");
 
   m.addEventListener("input", () => { f.checked = false; });
   f.addEventListener("input", () => { m.checked = false; });
-
-  // alert("Did you enter the correct name? An error was detected!");
-
   pageText.innerText = "Please complete the form below to get started...";
 
-  // male names list
-  const nMale = [
-    { name: "Noel" },
-    { name: "Joseph" },
-    { name: "Gabriel" },
-    { name: "Jesus" },
-    { name: "Rudolph" },
-    { name: "Ebenezer Scrooge" },
-    { name: "Buddy the Elf" },
-    { name: "Santa Claus" }
-  ];
-
-  // female names list
-  const nFemale = [
-    { name: "Mary" },
-    { name: "Carol Singer" },
-    { name: "Holly" },
-    { name: "Angel" },
-    { name: "Noelle" },
-    { name: "Ivy" }
-  ];
-
-
-
-  // todo: CREATE ANNOYING 'HURRY UP' POP-UPS
+  // todo: CREATE ANNOYING 'HURRY UP' POP-UPS or Alerts with setInterval()?
   // popup - 'please submit your information' 1 second after first input changes
   // const hurryUp = setTimeout(popHurry, 1000);
   // function popHurry() {
   // }
+  // alert("Did you enter the correct name? An error was detected!");
 
-  // todo: add annoying alerts with setInterval? -> alert("Did you enter the correct name? An error was detected!");
-
-  // todo: get telephone number after form submission, add number to form Object? Get from local storage
+  // todo: get telephone number after form submission, add number to local storage
   // teL.addEventListener("click", () => {
   //   teL.value = "999";
   //   const timer = setInterval(() => { teL.value = ""; alert("Thanks"); clearInterval(timer);}, 5000);
   //   popup - Thank you. Your telephone number has been submitted, we may use your number to contact you in the near future. We need this to confirm your personal information.
   // });
 
-  // todo: CREATE FAVOURITE MOVIE 
-  // from the information you have submitted and with the clever use of generative AI, we can determine that -
-  // your favourite Christmas movie is - the King's speech, Die Hard 2, Indiana Jones and the Temple of Doom, Mary Poppins
-
   // todo: ENTER TELEPHONE NUMBER TO CONFIRM  PERSONAL DETAILS
   // please enter your telephone number, change as entered, auto submit? fake submit? 999, 118 118, 111, 1471, 123, 101
   // submit(); reset();
+
+  // todo: CREATE FAVOURITE MOVIE?
+  // from the information you have submitted and with the clever use of generative AI, we can determine that -
+  // your favourite Christmas movie is - the King's speech, Die Hard 2, Indiana Jones and the Temple of Doom, Mary Poppins
 
   // todo: CREATE FAVOURITE CHRISTMAS PRESENT
   // your favourite Christmas present is -  
 
 
   dataForm.addEventListener("submit", (e) => {
+    butt[0].disabled = true;
     e.preventDefault(); // prevent data from showing in url
     const formData = new FormData(dataForm);
     const dataObj = Object.fromEntries(formData);
-    // console.log(dataObj);
 
     // get gender from form, select name from array
     let uSer, gEnder;
