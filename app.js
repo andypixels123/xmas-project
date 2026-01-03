2dd213bb562d406da23df044af596dc7b4c8f513
Andrew Birch <ab8218-cp@yahoo.com>
2026-01-03 17:48:56 +0000

    BUILD: backup mvp complete

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const savedData = localStorage.getItem("stats") ? localStorage.getItem("stats") : null;
-const pageText = document.getElementById("pageText");
 let userData = {};
 let autoName;
 const colours = ["blue", "pink", "green", "purple", "red", "black", "brown", "white", "gold", "orange", "silver", "beige", "yellow",];
@@ -16,23 +15,23 @@ const randomColour = colours[rNum(colours.length)];
 const randomAge = rNum(100) + 1;
 
 function getGift(age, gender) {
-  // presents ages 2-5, 6-12, 13-20, 21-35, 36-50, 50+
+  // presents, ages 2-5, 6-12, 13-20, 21-35, 36-50, 50+
   let i, gift;
   const maleGifts = [
-    ["Heavy Goods Vehicle", "VW Beetle", "shovel", "pet baboon", "uni-cycle", "pair of clown shoes", "spanner", "driving experience", "pipe and tobacco", "punch ball"],
-    ["bottle", "horror dvd", "walking stick", "pet reindeer", "bottle of whiskey", "flying Lesson", "bottle opener", "tea pot", "pair of walking boots", "hip flask"],
-    ["pet penguin", "air fryer", "cutlery set", "National Trust membership", "real polar bear", "pet lion", "log cabin", "cement mixer", "lawn mower", "scented candle"],
-    ["SpongeBob SquarePants", "toy ferrari", "wordsearch book", "jigsaw puzzle", "Scrabble Set", "calendar", "pen set", "can of oil", "drawing pad", "hair straightener"],
-    ["broom", "pogo stick", "pack of Paracetamol", "Thomas the Tank Engine", "colouring book", "crossword puzzle book", "bottle of diet pills", "Beano Annual", "Guinness Book of Records", "Space Hopper"],
-    ["colouring book", "action man", "pair of roller blades", "pair of clogs", "mini skirt", "plastic sheep", "personalised mug", "gym membership", "Lego set", "Play-Doh set"],
+    ["Heavy Goods Vehicle", "VW Beetle", "shovel", "pet baboon", "uni-cycle", "pair of clown shoes", "pet lion", "driving experience", "pipe and tobacco", "punch ball"],
+    ["fire extinguisher", "horror dvd", "walking stick", "pet reindeer", "bottle of whiskey", "flying Lesson", "bottle opener", "tea pot", "pair of walking boots", "hip flask"],
+    ["food mixer", "air fryer", "cutlery set", "National Trust membership", "real polar bear", "spanner", "log cabin", "barbeque tongs", "lawn mower", "scented candle"],
+    ["SpongeBob SquarePants", "toy ferrari", "wordsearch book", "jigsaw puzzle", "Scrabble Set", "calendar", "pen set", "personalised mug", "drawing pad", "hair straightener"],
+    ["broom", "pogo stick", "lipstick", "Thomas the Tank Engine", "colouring book", "crossword puzzle book", "diet pills", "Beano Annual", "Guinness Book of Records", "Space Hopper"],
+    ["colouring book", "action man", "pair of roller blades", "pair of clogs", "tricycle", "plastic sheep", "pet hamster", "gym membership", "Lego set", "Play-Doh set"],
   ];
   const femaleGifts = [
     ["Prosecco", "bottle of Gin", "hairdryer", "spa day", "comb", "food mixer", "manicure set", "bottle of Chanel No.5", "tin of biscuits", "tub of Quality Street"],
     ["set of hair curlers", "flower bouquet", "greenhouse", "chopping board", "socket set", "bottle of diet pills", "gym membership", "flower pot", "Slinky", "Nintendo Switch"],
     ["Dyson Vacuum Cleaner", "baking tray", "bricklaying course", "screwdriver set", "personalised toothbrush", "pair of novelty socks", "snack hamper", "bicycle tyre", "Cluedo Board Game", "portable power bank"],
     ["set of bowls", "cricket bat", "Furby", "Star Wars Power Crystal Lightsaber", "Hasbro Frustration Board Game", "Scalextric Set", "train set", "Airfix Kit", "Evel Knievel Stunt Set", "Stretch Armstrong"],
-    ["lipstick", "perm", "blue rinse", "self-help book", "radio-controlled car", "build-a-bear workshop", "Rubiks Cube", "Monopoly Game", "Sylvanian Families playset", "Spirograph Set"],
-    ["bicycle", "Barbie Doll", "pet hamster", "set of novelty underwear", "Fart Box", "Christmas jumper", "tricycle", "football", "Nintendo Game Boy", "Jenga Game"],
+    ["packet of paracetamol", "perm and blue rinse", "Jenga Game", "self-help book", "radio-controlled car", "build-a-bear workshop", "Rubiks Cube", "Monopoly Game", "Sylvanian Families playset", "Spirograph Set"],
+    ["bicycle", "Barbie Doll", "can of oil", "set of novelty underwear", "Fart Box", "Christmas jumper", "mini skirt", "football", "Nintendo Game Boy", "cement mixer"],
   ];
 
   switch (true) {
@@ -64,6 +63,7 @@ function getGift(age, gender) {
   }
   userData.userGift = gift;
   storeValues("stats", userData);
+  return gift;
 }
 
 // todo: make params semantic?
@@ -99,8 +99,10 @@ function removeElem(element, callBack) {
 
 function formTwo() {
   // e, aN, aT, pI, iTx, type, for, req
-  pageText.innerText = `Hello, ${userData.userName}.\r\n\nThank you for submitting your data.\r\n\nToday we will be collating information that you provide together with some very clever AI generated content for us to determine the perfect Christmas present for you.`;
   const elemGroup = createElem("span", "elemGroup", "", "", "", "", "", "");
+  createElem("p", "", "", "elemGroup", `Hello, ${userData.userName}.`, "", "", "");
+  createElem("p", "", "", "elemGroup", "Thank you for submitting your data.", "", "", "");
+  createElem("p", "", "", "elemGroup", "Today we will be collating the information that you provide together with some very clever AI generated content for us to determine the perfect Christmas present for you.", "", "", "");
   createElem("p", "", "", "elemGroup", "You will be pleased to know, you have passed our initial checks and you are now able to proceed and find your perfect Christmas present.", "", "", "");
   createElem("p", "", "", "elemGroup", "A few more steps are required....", "", "", "");
   const dataForm = createElem("form", "dataForm", "", "elemGroup", "", "", "", "");
@@ -118,11 +120,11 @@ function formTwo() {
     const formData = new FormData(dataForm);
     const dataObj = Object.fromEntries(formData);
 
-    // change form values
+    // change input values
     userInput.value = randomAge;
     colourInput.value = randomColour;
 
-    // store user data
+    // store data
     userData.userAge = randomAge;
     userData.userColour = randomColour;
     storeValues("stats", userData);
@@ -132,34 +134,50 @@ function formTwo() {
 
 function createGift() {
   // e, aN, aT, pI, iTx, type, for, req
-  pageText.innerText = `${userData.userName}, if you are not completely happy with the data you have submitted you may now amend your details, or get your perfect present!`;
+  createElem("p", "", "", "", `${userData.userName}, you will be pleased to know that we have found your perfect Christmas present. If, however, you are not completely satisfied with the data you have submitted, you may also amend your details below.`, "", "", "");
+  createElem("p", "", "", "", "Get your perfect Christmas present!", "", "", "");
+  // createElem("p", "", "", "", `${userData.userName}, if you are not completely happy with the data you have submitted you may now amend your details, or get your perfect present!`, "", "", "");
   // add telephone number js? We have your current location and together with the information you have provided, we are able to determine your telephone number. We have added this to your collated data and we may use this to contact you to confirm your personal details. Your telephone number is -
   // const teL = document.getElementById('telNum'); // telephone input
-  const amendBtn = createElem("div", "btn", "c", "", "Amend", "", "", "");
-  amendBtn.title = "amend personal details";
+
   const giftBtn = createElem("div", "btn", "c", "", "Present", "", "", "");
   giftBtn.title = "perfect present idea";
-  amendBtn.addEventListener("click", () => {
-    localStorage.removeItem("stats");
-    location.reload();
-  });
+  // const amendBtn = createElem("div", "btn", "c", "", "Amend", "", "", "");
+  // amendBtn.title = "amend personal details";
+  const resultP = createElem("p", "bold", "c", "", "", "", "", "");
+
   giftBtn.addEventListener("click", () => {
-    getGift(userData.userAge, userData.userGender);
-    // todo: show gift on page
+    const result = getGift(userData.userAge, userData.userGender);
+    resultP.textContent = `${result}`;
+    giftBtn.textContent = ("More");
   });
+
+  // amendBtn.addEventListener("click", () => {
+  //   const result = getGift(userData.userAge, userData.userGender);
+  //   resultP.textContent = `${result}`;
+  // localStorage.removeItem("stats");
+  // location.reload();
+  // });
+
 }
 
 if (savedData) {
-  // data found in local storage
+  // data found, local storage
   try {
     userData = JSON.parse(savedData);
     console.log(userData);
   } catch (err) {
-    console.error(`local storage error: ${err}`);
+    console.error(`local storage error: ${err} `);
   }
-  // todo: remove 'pageText' -> create <p> elements dynamically
-  pageText.innerText = `Welcome back ${userData.userName}!\r\n\nFrom your previous visit we harvested the following personal information:\r\n\nYour name is ${userData.userName}\r\nYour favourite colour is ${userData.userColour}\r\nYou are ${userData.userAge} years of age\r\nYour gender is ${userData.userGender}\r\n\nWith the information you have provided and our clever use of generative AI, we found your perfect Christmas present:`;
-  createElem("p", "bold", "c", "", `${userData.userGift}`, "", "", "");
+
+  createElem("p", "", "", "", `Welcome back ${userData.userName} !`, "", "", "");
+  createElem("p", "", "", "", "From your previous visit we harvested the following personal information:", "", "", "");
+  createElem("p", "no-margin", "c", "", `Your name is ${userData.userName}`, "", "", "");
+  createElem("p", "no-margin", "c", "", `Your favourite colour is ${userData.userColour}`, "", "", "");
+  createElem("p", "no-margin", "c", "", `You are ${userData.userAge} years of age`, "", "", "");
+  createElem("p", "", "", "", `Your gender is ${userData.userGender}`, "", "", "");
+  createElem("p", "", "", "", "With the information you have provided and our clever use of generative AI, we found your perfect Christmas present:", "", "", "");
+  createElem("p", "bold", "c", "", `${userData.userGift} `, "", "", "");
   const amendBtn = createElem("div", "btn", "c", "", "Amend", "", "", "");
   amendBtn.title = "amend personal details";
   amendBtn.addEventListener("click", () => {
@@ -170,7 +188,9 @@ if (savedData) {
 } else {
   // local storage empty -> create form
   // e, aN, aT, pI, iTx, type, for, req
-  const dataForm = createElem("form", "dataForm", "", "", "", "", "", "");
+  const elemGroup = createElem("span", "elemGroup", "", "", "", "", "", "");
+  createElem("p", "", "", "elemGroup", "Please complete the form below to get started...", "", "", "");
+  const dataForm = createElem("form", "dataForm", "", "elemGroup", "", "", "", "");
   createElem("label", "", "", "dataForm", "What is your name?", "", "userName", "");
   const userInput = createElem("input", "userName", "", "dataForm", "", "text", "", "y");
   const checkboxP = createElem("p", "", "", "dataForm", "", "", "", "");
@@ -182,12 +202,12 @@ if (savedData) {
   createElem("button", "", "", "dataForm", "submit", "submit", "", "");
   const buttons = document.getElementsByTagName("button");
 
-  // todo: make autoname randomly male or female
-  // annoying auto-fill on name field 
   userInput.addEventListener("input", () => {
+    // annoying auto fill input field
     let nameGroup = rNum(userNames.length);
     autoName = userNames[nameGroup][rNum(userNames[nameGroup].length)];
     userInput.value = autoName;
+    // alert("Did you enter the correct name? An error was detected!");
   });
 
   maleCheckbox.addEventListener("input", () => {
@@ -198,8 +218,6 @@ if (savedData) {
     maleCheckbox.checked = false;
   });
 
-  pageText.innerText = "Please complete the form below to get started...";
-
   dataForm.addEventListener("submit", (e) => {
     buttons[0].disabled = true;
     e.preventDefault(); // prevent data from showing in url
@@ -207,48 +225,43 @@ if (savedData) {
     const dataObj = Object.fromEntries(formData);
 
     // get gender from form
-    let uSer, gEnder;
+    let gEnder;
     autoName = userInput.value;
 
     switch ("on") {
       case dataObj.inputMale:
-        uSer = autoName;
         gEnder = "male";
         break;
       case dataObj.inputFemale:
-        uSer = autoName;
         gEnder = "female";
         break;
       default:
-        uSer = autoName;
         gEnder = "unconfirmed";
     }
 
-    // change form values
-    userInput.value = uSer;
+    // change input value
+    userInput.value = autoName;
 
-    // store user data
-    userData.userName = uSer;
+    // store data
+    userData.userName = autoName;
     userData.userGender = gEnder;
     storeValues("stats", userData);
 
     // delay, optionally run another function after removing element
-    removeElem(dataForm, () => setTimeout(formTwo, 5100));
+    removeElem(elemGroup, () => setTimeout(formTwo, 5100));
     // removeElem(dataForm, null); // remove element, no callback
     // removeElem(dataForm, () => setTimeout(console.log("run new function"), 6000)); // test callback
     // also removes event handlers
   });
 }
 
-// todo: SHOW FAVOURITE CHRISTMAS PRESENT ON PAGE, IN POP-UP??
-// todo: add sound, fanfare?
-
+// todo: SHOW FAVOURITE CHRISTMAS PRESENT ON PAGE, add sound, fanfare?
 // todo: CREATE ANNOYING 'HURRY UP' POP-UPS or Alerts with setInterval()?
 // popup - 'please submit your information' 1 second after first input changes
 // const hurryUp = setTimeout(popHurry, 1000);
 // function popHurry() {
+// alert("Please enter your name to get started");
 // }
-// alert("Did you enter the correct name? An error was detected!");
 
 // todo: Create fake telephone number after form submission? Add to local storage?
 // teL.addEventListener("click", () => {
